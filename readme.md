2b34b16b9c3122410f7a2ea3447697fb32466a38
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-04-21 13:26:46 +0200

    updated to v1.2.0

--- readme.md
+++ readme.md
@@ -1,28 +1,21 @@
 # File handler
 
-## Example for table handler
+## Example for simple table handler
 
 ```ts
 import { Handler } from "@ghom/handler"
 
-export const tableHandler = new Handler(path.join(process.cwd(), "dist", "tables"))
-
-tableHandler.once("finish", async (pathList) => {
-  const tables = await Promise.all(
-    pathList.map(async (filepath) => {
-      const file = await import("file://" + filepath)
-      if (filepath.endsWith(".native.js")) file.default.options.native = true
-      file.default.filepath = filepath
-      return file.default
-    })
-  )
-
-  return Promise.all(
-    tables
-      .sort((a, b) => {
-        return (b.options.priority ?? 0) - (a.options.priority ?? 0)
-      })
-      .map((table) => table.make())
-  )
+export const handler = new Handler("dist/table", {
+  logger: console,
+  loggerPattern: "loaded new table: $filename",
+  loader: (path) => import(`file://${path}`),
 })
+
+try {
+  await handler.init()
+} catch(e) {
+  console.error(e)
+}
+
+console.log(handler.elements)
 ```
--- src/app/handler.ts
+++ src/app/handler.ts
@@ -8,17 +8,64 @@ export interface HandlerEvents extends BaseEventNames {
   finish: [pathList: string[]]
 }
 
-export class Handler extends EventEmitter<HandlerEvents> {
-  constructor(private path: string) {
+export interface HandlerOptions<Element> {
+  logger?: {
+    log: (message: string) => void
+  }
+  /**
+   * Use $path to replace by file path <br>
+   * Use $basename to replace by file name <br>
+   * Use $filename to replace by file name without extension <br>
+   * @example ```ts
+   * const handler = new Handler("./commands", {
+   *   loggerPattern: "$filename loaded"
+   *   logger: console
+   * })
+   * ```
+   */
+  loggerPattern?: string
+  loader?: (path: string) => Promise<Element>
+}
+
+export class Handler<Element> extends EventEmitter<HandlerEvents> {
+  public elements: Map<string, Element> = new Map()
+
+  constructor(private path: string, private options?: HandlerOptions<Element>) {
     super()
   }
 
+  /**
+   * Here to prevent breaking changes.
+   * @deprecated Use `load` instead.
+   */
   async load() {
+    await this.init()
+  }
+
+  async init() {
+    this.elements.clear()
+
     const filenames = await fs.readdir(this.path)
     const filepathList: string[] = []
-    for (const filename of filenames) {
-      const filepath = path.join(this.path, filename)
+    for (const basename of filenames) {
+      const filepath = path.join(this.path, basename)
+      const filename = path.basename(filepath, path.extname(filepath))
+
       filepathList.push(filepath)
+
+      if (this.options?.logger)
+        this.options.logger.log(
+          this.options.loggerPattern
+            ? this.options.loggerPattern
+                .replace("$path", filepath)
+                .replace("$basename", basename)
+                .replace("$filename", filename)
+            : `loaded ${filename}`
+        )
+
+      if (this.options?.loader)
+        this.elements.set(filepath, await this.options.loader(filepath))
+
       await this.emit("load", filepath)
     }
     await this.emit("finish", filepathList)
